09a4804e7ae89be1e17eb59d551f7bfa770b7a23
RamonPaiva <ramon.mesquita@gmail.com>
2018-09-30 21:06:49 -0300

    Adequando o template gerado e mais data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ALURAPIC';
+  description = 'Leão';
+  url = 'https://http2.mlstatic.com/resident-evil-4-pc-completo-legendado-portugus-c-brinde-D_NQ_NP_643205-MLB26510344008_122017-F.jpg';
 }
