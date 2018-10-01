c7af9adcb5c1be64fa4433de979692f51683158f
RamonPaiva <ramon.mesquita@gmail.com>
2018-09-30 22:02:00 -0300

    Criando o primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ALURAPIC';
-  description = 'Leão';
-  url = 'https://http2.mlstatic.com/resident-evil-4-pc-completo-legendado-portugus-c-brinde-D_NQ_NP_643205-MLB26510344008_122017-F.jpg';
+  
 }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+    url = 'https://http2.mlstatic.com/resident-evil-4-pc-completo-legendado-portugus-c-brinde-D_NQ_NP_643205-MLB26510344008_122017-F.jpg';
+    description = 'Resident Evil 4';
+}
