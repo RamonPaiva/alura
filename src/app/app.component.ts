1b96bc797973d87161dc75dfa718ffcaaf9331c9
RamonPaiva <ramon.mesquita@gmail.com>
2018-10-04 22:13:06 -0300

    HttpClient e injeção dependência

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
-    {
-        url: 'https://http2.mlstatic.com/resident-evil-4-pc-completo-legendado-portugus-c-brinde-D_NQ_NP_643205-MLB26510344008_122017-F.jpg',
-        description: 'RE4'
-    },
-    {
-        url: 'http://proximonivel.pt/wp-content/uploads/2016/12/resident-evil-5-remaster-rev-top-pn.jpg',
-        description: 'RE5'
-    }
-  ];
+  photos = [];
+  
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 
 }
